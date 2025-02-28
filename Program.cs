644f17148130929d6c7a7de3d0789d10f848eb15
Ilya Boiko <boiko.ilya.evg@gmail.com>
2025-02-28 09:55:29 +0500

    12.1.5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFyCSm012p1e5;
+
+class Program
+{
+
+    /// <summary>
+    /// Метод реализущий показ рекламы
+    /// </summary>
+    static void ShowAds()
+    {
+        var ads = new[]
+        {
+            "Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com",
+            "Купите подписку на МыКомбо и слушайте музыку везде и всегда.",
+            "Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу."
+        };
+
+        foreach (var ad in ads)
+        {
+            Console.WriteLine(ad);
+
+            Thread.Sleep(500);
+        }
+    }
+
+    /// <summary>
+    /// Процедура реализующая основной алгоритм работы приложения.
+    /// </summary>
+    static void PerformMain()
+    {
+        List<User> users = new List<User>()
+        {
+            new() {Name = "Пользователь 1", Login = "User1", IsPremium = true},
+            new() {Name = "Пользователь 2", Login = "User2", IsPremium = false},
+            new() {Name = "Пользователь 3", Login = "User3", IsPremium = false},
+        };
+
+        foreach (var user in users)
+        {
+            Console.WriteLine("Приветствую {0}", user.Name);
+
+            if (!user.IsPremium)
+                ShowAds();
+        }
+    }
+
+    /// <summary>
+    /// Главная точка входа приложения
+    /// </summary>
+    /// <param name="args">Аргументы командной строки при запуске приложения.</param>
+    static void Main(string[] args)
+    {
+        PerformMain();
+    }
+}
---
+++ User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFyCSm012p1e5;
+
+/// <summary>
+/// Класс, представляющий пользователя
+/// </summary>
+public class User
+{
+    /// <summary>
+    /// Логин пользователя
+    /// </summary>
+    public string? Login { get; set; }
+
+    /// <summary>
+    /// Имя пользователя
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Признак премиум статуса
+    /// </summary>
+    public bool IsPremium { get; set; }
+
+    /// <summary>
+    /// Инициализирует новый экземпляр класса.
+    /// </summary>
+    public User()
+    {
+        // Конструктор по умолчанию
+    }
+}
